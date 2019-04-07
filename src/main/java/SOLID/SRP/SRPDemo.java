2c772bd0405a7efbd96630a48c57a87382e20355
ephemeralin <ephemeralin@gmail.com>
2019-04-07 22:17:39 +0300

    Add SOLID SRP

---
+++ src/main/java/SOLID/SRP/SRPDemo.java
@@ -0,0 +1,14 @@
+package SOLID.SRP;
+
+public class SRPDemo {
+    public static void main(String[] args) throws Exception {
+        Journal j = new Journal();
+        j.addEntry("I cried today");
+        j.addEntry("I ate a bug");
+        System.out.println(j);
+
+        Persistence p = new Persistence();
+        String filename = "journal.txt";
+        p.saveToFile(j, filename, true);
+    }
+}
